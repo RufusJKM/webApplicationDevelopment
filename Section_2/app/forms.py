0bc34bf9b18eb1e27b39ab5b468a296e0d9eb57f
RufusJKM <146677707+RufusJKM@users.noreply.github.com>
2024-10-11 13:20:18 +0000

    Trying section 5 task

--- Section_2/app/forms.py
+++ Section_2/app/forms.py
@@ -1,7 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField
+from wtforms import StringField
+from wtforms import DecimalField
 from wtforms.validators import DataRequired
 
 class CalculatorForm(FlaskForm):
     number1 = IntegerField('number1', validators=[DataRequired()])
     number2 = IntegerField('number2', validators=[DataRequired()])
+
+class PropertyForm(FlaskForm):
+    address = StringField('Address', validators=[DataRequired()])
+    duration = IntegerField('Duration', validators=[DataRequired()])
+    rent = DecimalField('Rent', validators=[DataRequired()])
--- Section_2/app/views.py
+++ Section_2/app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash
-from app import app
-from .forms import CalculatorForm
+from app import app, db, models
+import datetime
+from .forms import CalculatorForm, PropertyForm
 
 @app.route('/fruit')
 def displayFruit():
@@ -15,3 +16,24 @@ def calculator():
     return render_template('calculator.html',
                            title='Calculator',
                            form=form)
+
+@app.route('/new', methods=['GET', 'POST'])
+def new():
+    form = PropertyForm()
+    if form.validate_on_submit():
+        flash('Successfully entered property details')
+        p = models.Property(address=form.address.data,start_date=datetime.datetime.utcnow(),duration=form.duration.data, rent=form.rent.data)
+        db.session.add(p)
+        db.session.commit()
+    return render_template('new.html',
+                            title='New Property',
+                            form=form)
+@app.route('/details', methods=['GET', 'POST'])
+def details():
+    properties = []
+    for p in models.Property.query.all():
+        properties.append(p)
+    return render_template('details.html',
+                            title='Properties',
+                            properties=properties)
+
