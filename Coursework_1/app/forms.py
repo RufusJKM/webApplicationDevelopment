93b1d5fffbeac5b6f8172e4e42391e877c329a4a
RufusJKM <146677707+RufusJKM@users.noreply.github.com>
2024-10-24 15:39:00 +0000

    ready for submission

--- Coursework_1/app/forms.py
+++ Coursework_1/app/forms.py
@@ -1,19 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField
-from wtforms import StringField
-from wtforms import BooleanField
-from wtforms import DateField
-from wtforms import RadioField
-from wtforms import SelectField
+from wtforms import IntegerField, StringField, BooleanField, DateField, SelectField
 from wtforms.validators import DataRequired
 from app import app, db, models
 
 #Form used to create a new assessment
+#Reference for render_kw: Stack Overflow. [Online]. [Accessed 24/10/2024]. Available from https://stackoverflow.com/questions/64820785/can-you-adjust-the-width-of-a-question-in-flask-wtforms
 class NewAssessmentForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     code = IntegerField('Code', validators=[DataRequired()])
     dueDate= DateField('Date', validators=[DataRequired()])
-    description = StringField('Description', validators=[DataRequired()])
+    description = StringField('Description', render_kw={'size': 60}, validators=[DataRequired()])
     completed = BooleanField('Completed')
 
 #Form used to filter results by complete/incomplete
@@ -25,12 +21,11 @@ class ChooseForm(FlaskForm):
     chooseAssessment = IntegerField('chooseAssessment', validators=[DataRequired()])
     
 #Form to change details of an assessment
-#Reference for render_kw: Stack Overflow. [Online]. [Accessed 24/10/2024]. Available from https://stackoverflow.com/questions/64820785/can-you-adjust-the-width-of-a-question-in-flask-wtforms
 class EditForm(FlaskForm):
     #The hidden field is used to hold the id of the chosen assessment
     hidden = IntegerField('Hidden', validators=[DataRequired()])
     title = StringField('Title', validators=[DataRequired()])
     code = IntegerField('Code', validators=[DataRequired()])
     dueDate= DateField('Date', validators=[DataRequired()])
-    description = StringField('Description', render_kw={'size': 75}, validators=[DataRequired()])
+    description = StringField('Description', render_kw={'size': 60}, validators=[DataRequired()])
     completed = BooleanField('Completed')
