2c5091336d6282f6f1275bf97f7845d6d491f17d
RufusJKM <146677707+RufusJKM@users.noreply.github.com>
2024-11-18 13:42:35 +0000

    Section 8 failed

--- section7_startpoint/app/__init__.py
+++ section7_startpoint/app/__init__.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_admin import Admin, request, session
+from flask_babel import Babel
+
+def get_locale():
+    if request.args.get('lang'):
+        session['lang'] = request.args.get('lang')
+    return session.get('lang', 'en')
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 # Handles all migrations.
 migrate = Migrate(app, db)
+
+babel = Babel(app, locale_selector=get_locale)
+admin = Admin(app,template_mode='bootstrap4')
 
 from app import views, models
 
--- section7_startpoint/app/views.py
+++ section7_startpoint/app/views.py
@@ -1,4 +1,9 @@
-from app import app
+from app import app, db, admin
+from flask_admin.contrib.sqla import ModelView
+from .models import Property, Landlord
+
+admin.add_view(ModelView(Property, db.session))
+admin.add_view(ModelView(Landlord, db.session))
 
 @app.route('/')
 def index():
