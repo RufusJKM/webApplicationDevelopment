1f33798b27944c3729c26f7d944a2574d1a3daa5
RufusJKM <146677707+RufusJKM@users.noreply.github.com>
2024-11-20 14:32:28 +0000

    Restarted cwk2

--- Coursework_2/app/models.py
+++ Coursework_2/app/models.py
@@ -2,42 +2,45 @@ from app import db
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50), index=True, unique=True)
     imgurl = db.Column(db.String(500))
+    category = db.Column(db.String(50))
     price = db.Column(db.Float)
     count = db.Column(db.Integer)
-    orders = db.relationship('Order', backref='product', lazy='dynamic')
 
     def __repr__(self):
-            return '{}{}{}{}{}'.format(self.id, self.name, self.imgurl, self.price, self.count)
+            return '{}{}{}{}{}'.format(self.id, self.name, self.imgurl, self.category, self.price, self.count)
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(20))
+    first_name = db.Column(db.String(20), index=True)
     last_name = db.Column(db.String(20))
     phone_number = db.Column(db.String(20))
     email = db.Column(db.String(50))
     username = db.Column(db.String(20))
     password = db.Column(db.String(20))
-    cards = db.relationship('Card', backref='customer', lazy='dynamic')
-    orders = db.relationship('Order', backref='customer', lazy='dynamic')
 
     def __repr__(self):
             return '{}{}{}{}{}{}{}'.format(self.id, self.first_name, self.last_name, self.phone_number, self.email, self.username, self.password)
 
 class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    number = db.Column(db.String, unique=True)
+    number = db.Column(db.String, index=True, unique=True)
     expirery = db.Column(db.DateTime)
     cvv = db.Column(db.Integer)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'))
 
     def __repr__(self):
             return '{}{}{}{}'.format(self.id, self.number, self.expirery, self.cvv)
 
-class Order(db.Model):
+class Basket(db.Model):
     customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), primary_key=True)
     product_id = db.Column(db.Integer, db.ForeignKey('product.id'), primary_key=True)
 
-    def __repr__(self):
+    def __repr__(self): 
             return '{}{}'.format(self.customer_id, self.product_id)
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    def __repr__(self): 
+            return '{}'.format(self.id)
