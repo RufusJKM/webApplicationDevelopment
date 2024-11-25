132445a84f3708648b0e0512d439c0997b35885c
RufusJKM <146677707+RufusJKM@users.noreply.github.com>
2024-11-25 19:03:01 +0000

    Started home page

---
+++ Coursework_2/app/static/homeScript.js
@@ -0,0 +1,12 @@
+document.getElementById('quantity').addEventListener('change', function() {
+    // Get the selected value
+    const val = this.options[this.selectedIndex].value;
+    
+    price = document.getElementById('price')
+    
+  });
+
+function addToBasket() {
+    dropdown = document.getElementById("quantity")
+
+}
